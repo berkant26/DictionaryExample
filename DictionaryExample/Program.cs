b116c2c4aaff1cf22dfbf3f60c4ae4c6a3240b27
berkant26 <youbeko26@gmail.com>
2021-08-17 11:04:51 +0300

    Add project files.

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> AddCity = new MyDictionary<string, int>();
+            AddCity.Add("eskisehir", 26);
+            AddCity.Add("New yoruk", 01);
+            AddCity.Add("Dortmund", 25);
+            Console.WriteLine(AddCity.Count);
+
+
+
+
+        }
+    }
+}
